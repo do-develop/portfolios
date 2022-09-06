449044d045369dccbd86b7edc26b4cfcc45280a2
unknown <doyoh1179@gmail.com>
2022-09-06 21:52:12 +0200

    used Sanity backend for About page

--- fullstack/frontend_react/src/container/About/About.jsx
+++ fullstack/frontend_react/src/container/About/About.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import './About.scss';
 import {images} from '../../constants';
+import {urlFor, client} from '../../client';
 
-const abouts = [
-  { title: 'Cybersecurity', description: 'I have double majored Cybersecurity and Computer Science. I have an CEH certificate.', imgUrl: images.about01 },
-  { title: 'Energy Informatics', description: 'I am interested in how mathematical optimization in the context of computer science can contribute to an effective management of smart energy and power systems', imgUrl: images.about02 },
-]
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => { setAbouts(data); })
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>I am your <span>Good Developer</span> <br /> and <span>Good Coworker</span>
@@ -22,7 +27,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title}/>
+            <img src={urlFor(about.imgUrl)} alt={about.title}/>
             <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
             <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
           </motion.div>
